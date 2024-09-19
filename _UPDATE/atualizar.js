69b3ff51056579df573dbda8362a52e0d150724d
Gabriel Augusto <33496735+gabaugusto@users.noreply.github.com>
2024-09-19 08:00:30 -0300

    Update atualizar.js

--- _UPDATE/atualizar.js
+++ _UPDATE/atualizar.js
@@ -1,4 +1,4 @@
-// alterar o nome de um único médico
+// Alterar o nome de um único médico
 db.medicos.updateOne({
     nome: "Dra. Carla Elizabete"
 }, {
@@ -7,11 +7,30 @@ db.medicos.updateOne({
     }
 });
 
-// alterar o nome de todos os médicos que tenham "Infectologia" como especialidade
+// Alterar o nome de todos os médicos que tenham "Infectologia" como especialidade
 db.medicos.updateMany({
     especialidades: "Infectologia"
 }, {
     $set: {
         nome: "Dr(a). Infectologista"
+    }
+});
+
+// Alterar o nome de todos os médicos que tenham o CRM como documentos.CRM e adicionando um novo campo
+db.medicos.updateOne({
+    "documentos.CRM": 'MG234567'
+}, {
+    $set: {
+        nome: "Dra. Carla Bete",
+        "documentos.Passaporte": 'A1B2C3D4E5F6',
+    }
+});
+
+// Remover um campo é simples: basta usar o operador $unset
+db.medicos.updateOne({
+    nome: "Dra. Carla Bete"
+}, {
+    $unset: {
+        "documentos.Passaporte": ""
     }
 });
