b0f88d3ecea27ed7f6d37fab545f8dbad631f791
Gabriel Augusto <33496735+gabaugusto@users.noreply.github.com>
2024-10-01 10:41:03 -0300

    Removing Stuff

--- 1_CREATE/db.quartos.js
+++
@@ -1,8 +0,0 @@
-db.quartos.insertMany([
-    { numero: "101", tipo: "Apartamento", valor_diario: 300
-    },
-    { numero: "102", tipo: "Quarto Duplo", valor_diario: 200
-    },
-    { numero: "103", tipo: "Enfermaria", valor_diario: 150
-    }
-]);
