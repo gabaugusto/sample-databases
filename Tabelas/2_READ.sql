59397e603ec5d2df11dd72b74cd65d4eb7fc952e
Gabriel Augusto <33496735+gabaugusto@users.noreply.github.com>
2024-10-01 11:30:10 -0300

    Update

--- Tabelas/2_READ.sql
+++ Tabelas/2_READ.sql
@@ -16,6 +16,10 @@ SELECT * FROM nome_da_tabela WHERE nome_da_coluna = valor;
 
 SELECT * FROM nome_da_tabela WHERE nome_da_coluna1 = valor1 AND nome_da_coluna2 = valor2;
 
+-- Selecionar registros de uma tabela com uma condição de comparação de texto
+
+SELECT * FROM nome_da_tabela WHERE nome_da_coluna LIKE '%valor%';
+
 -- Selecionar registros de uma tabela com uma condição de comparação maior que 
 
 SELECT * FROM nome_da_tabela WHERE nome_da_coluna > valor;
@@ -28,9 +32,7 @@ SELECT * FROM nome_da_tabela WHERE nome_da_coluna BETWEEN valor1 AND valor2;
 
 SELECT * FROM nome_da_tabela WHERE nome_da_coluna IN (valor1, valor2, valor3);
 
--- Selecionar registros de uma tabela com uma condição de comparação de texto
 
-SELECT * FROM nome_da_tabela WHERE nome_da_coluna LIKE 'valor%';
 
 -- Selecionar registros de uma tabela com uma condição de comparação de texto nula
 
@@ -40,3 +42,37 @@ SELECT * FROM nome_da_tabela WHERE nome_da_coluna IS NULL;
 
 SELECT * FROM nome_da_tabela WHERE nome_da_coluna IS NOT NULL;
 
+-- Selecionar registros de uma tabela com uma condição de comparação de texto ignorando maiúsculas e minúsculas
+
+SELECT * FROM nome_da_tabela WHERE UPPER(nome_da_coluna) LIKE '%VALOR%';
+
+
+-- ###########################################################################################
+-- ###########################################################################################
+-- ###########################################################################################
+
+-- JOINS
+
+-- Selecionar registros de duas tabelas
+
+SELECT * FROM nome_da_tabela_numero_um
+INNER JOIN nome_da_outra_tabela
+ON nome_da_tabela_numero_um.nome_da_coluna = nome_da_outra_tabela.nome_da_coluna;
+
+-- Selecionar registros de duas tabelas com uma condição
+
+SELECT * FROM nome_da_tabela_numero_um
+INNER JOIN nome_da_outra_tabela
+ON nome_da_tabela_numero_um.nome_da_coluna = nome_da_outra_tabela.nome_da_coluna
+WHERE nome_da_tabela_numero_um.nome_da_coluna = valor;
+
+-- Diferença entre INNER JOIN e LEFT JOIN
+
+-- INNER JOIN: Retorna apenas os registros que possuem correspondência nas duas tabelas
+-- LEFT JOIN: Retorna todos os registros da tabela da esquerda e os registros correspondentes da tabela da direita
+
+-- Selecionar registros de duas tabelas com LEFT JOIN
+
+SELECT * FROM nome_da_tabela_numero_um
+LEFT JOIN nome_da_outra_tabela
+ON nome_da_tabela_numero_um.nome_da_coluna = nome_da_outra_tabela.nome_da_coluna;
---
+++ Tabelas/5_VIEWS.sql
@@ -0,0 +1,10 @@
+-- VIEWS são consultas salvas no banco de dados. Elas são úteis para simplificar consultas complexas e para restringir o acesso a determinados dados.
+
+-- -- Criando uma view
+
+CREATE VIEW nome_da_view AS exemplo_de_view
+SELECT nome_da_coluna1, nome_da_coluna2
+FROM nome_da_tabela_numero_um 
+INNER JOIN nome_da_outra_tabela
+ON nome_da_tabela_numero_um.nome_da_coluna = nome_da_outra_tabela.nome_da_coluna;
+
